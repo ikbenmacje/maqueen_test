de635bb9aa7cabab2782d3b2da298f4e01369351
Machiel Veltkamp <machiel@z25.org>
2023-01-29 15:24:11 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showLeds(`
+    # # . # #
+    . # . # .
+    . # # # .
+    . . # . .
+    . . # . .
+    `)
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    basic.pause(2000)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    basic.pause(100)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
+})
